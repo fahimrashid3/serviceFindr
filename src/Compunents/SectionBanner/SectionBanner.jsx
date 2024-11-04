0772e371afbe328001ccc6976b7e010b5b987542
Your Name <fahimrashid350@gmail.com>
2024-11-04 20:12:56 +0600

    dynamic section banner created

---
+++ src/Compunents/SectionBanner/SectionBanner.jsx
@@ -0,0 +1,21 @@
+/* eslint-disable react/prop-types */
+
+const SectionBanner = ({ img, title, descriptions }) => {
+  return (
+    <div
+      className="lg:px-[15%] md:px-[10%] px-[5%] lg:py-20 md:py-12 py-7 lg:mt-20 md:mt-12 mt-7 rounded-lg bg-cover"
+      style={{ backgroundImage: `url(${img})` }}
+    >
+      <div
+        className="
+      bg-white dark:bg-dark-800 text-dark-900 dark:text-white lg:p-20 md:p-12 p-7 lg:space-y-8 md:space-y-5 space-y-3 rounded-lg opacity-85
+      "
+      >
+        <h1 className="text-4xl text-center lg:font-semibold">{title}</h1>
+        <p className="text-center lg:px-5">{descriptions}</p>
+      </div>
+    </div>
+  );
+};
+
+export default SectionBanner;
--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,11 +1,21 @@
+import SectionBanner from "../../../Compunents/SectionBanner/SectionBanner";
 import Carousel from "../Carousel/Carousel";
 import Categories from "../Categories/Categories";
+import img from "../../../assets/home/HomePageBanner.png";
 
 const Home = () => {
+  const title = "ServiceFinder";
+  const descriptions =
+    "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.";
   return (
     <div>
       <Carousel></Carousel>
       <Categories></Categories>
+      <SectionBanner
+        img={img}
+        title={title}
+        descriptions={descriptions}
+      ></SectionBanner>
     </div>
   );
 };
